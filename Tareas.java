e0abb281571be755d20fe84a5b863a30d380da96
gabohg <143622336+gabohg@users.noreply.github.com>
2023-09-06 08:41:23 -0600

    Add files via upload

---
+++ Tareas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg5.tareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "mapa mental,";
+        String tarea2 = " cuadro sinoptico,";
+        String tarea3 = " sinopsis,";
+        String tarea4 = " un reporte,";
+        String tarea5 = " una exposicion.";
+        
+        System.out.println("mis tareas son: " + tarea1 + tarea2 + tarea3 + tarea4 + tarea5);
+    }
+    
+}
---
+++ Variable.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variable;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Variable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        String nombre = "gabriel";
+        String materia = "fundamentos de la programacion";
+        String nombremascota = "gatote";
+        final int anionacimiento = 2005;
+        
+        String cadenaTexto = "cadena de texto!!! %% $$$";
+        
+        boolean prederluz = true;
+        boolean apagarluz = false;
+        
+        edad = 25;
+        edad = 50;
+        
+        System.out.println("edad: " + edad);
+        
+        System.out.println("Hola mi nombre es: " + nombre);
+        System.out.println("(Hola mi nombre es :" + nombre + " y estudio " + materia);
+    }
+    
+}
